6461dd72604918cd760736448b9bbb24d85481fb
kamil.komnacki <kamilkomnacki@gmail.com>
2020-06-05 21:57:27 +0200

    Finish layout and validators

--- app/src/main/java/com/komnacki/androidspyapp/MainActivity.kt
+++ app/src/main/java/com/komnacki/androidspyapp/MainActivity.kt
@@ -7,14 +7,12 @@ import android.content.IntentFilter
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.text.format.Time
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.google.android.gms.common.api.GoogleApi
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -35,32 +33,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_AppCompat)
-        setContentView(R.layout.activity_main)
         Log.d("SERVICE: ", "main activity onCreate  ")
 
-//        val receiverFilter = IntentFilter(Intent.ACTION_CONFIGURATION_CHANGED)
-//        val receiver = ServiceStarter()
-//        registerReceiver(receiver, receiverFilter)
+        setContentView(R.layout.activity_main)
 
-        //FIREBASE:
-//        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().reference
+        var isEmailValid = false
+        var isPasswordValid = false
+        et_email.addTextChangedListener (object:TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                isEmailValid = validateEmail(s.toString())
+                btn_connect.isEnabled = isEmailValid && isPasswordValid
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        et_password.addTextChangedListener (object:TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                isPasswordValid = !s.toString().isBlank()
+                btn_connect.isEnabled = isEmailValid && isPasswordValid
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
     }
 
-    override fun onCreateView(
-        parent: View?,
-        name: String,
-        context: Context,
-        attrs: AttributeSet
-    ): View? {
-        val rootView = super.onCreateView(parent, name, context, attrs)
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        val rootView = super.onCreateView(name, context, attrs)
+
+        return rootView
+    }
+
+//    override fun onCreateView(
+//        parent: View?,
+//        name: String,
+//        context: Context,
+//        attrs: AttributeSet
+//    ): View? {
+//        val rootView = super.onCreateView(parent, name, context, attrs)
+//        setContentView(R.layout.activity_main)
+//
+//        var isEmailValid = false
+//        var isPasswordValid = false
 //        et_email.addTextChangedListener (object:TextWatcher{
 //            override fun afterTextChanged(s: Editable?) {
+//                isEmailValid = validateEmail(s.toString())
+//                btn_connect.isEnabled = isEmailValid && isPasswordValid
 //            }
 //
 //            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 //
 //            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 //        })
-        return rootView
+//        et_password.addTextChangedListener (object:TextWatcher{
+//            override fun afterTextChanged(s: Editable?) {
+//                isPasswordValid = !s.toString().isBlank()
+//                btn_connect.isEnabled = isEmailValid && isPasswordValid
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//        })
+//        return rootView
+//    }
+
+    fun validateEmail(input: String) : Boolean {
+        return input.matches(Regex("[a-zA-Z0-9._-]+@[a-z]+.[a-z]+"))
     }
 }
