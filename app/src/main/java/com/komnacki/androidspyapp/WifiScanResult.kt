a32b8302cd521b5dd7f99e94b1ec1bb4c88438c8
kamil.komnacki <kamilkomnacki@gmail.com>
2021-02-02 19:41:58 +0100

    Move string properties to val's

--- app/src/main/java/com/komnacki/androidspyapp/WifiScanResult.kt
+++ app/src/main/java/com/komnacki/androidspyapp/WifiScanResult.kt
@@ -12,16 +12,22 @@ class WifiScanResult(override var context: Context, scanResult: ScanResult?) : M
     }
 
     private lateinit var info : MutableMap<String, Any>
+    private val CAPABILITIES_PROPERTY_NAME = "CAPABILITIES"
+    private val SSID_PROPERTY_NAME = "SSID"
+    private val BSSID_PROPERTY_NAME = "BSSID"
+    private val FREQUENCY_PROPERTY_NAME = "FREQUENCY"
+    private val LEVEL_PROPERTY_NAME = "LEVEL"
+    private val UNKNOWN_PROPERTY_NAME = "UNKNOWN_"
 
     private fun getWifiResults(scanResult: ScanResult?) {
         if(scanResult != null) {
             try {
                 info = mutableMapOf()
-                info["CAPABILITIES"] = scanResult.capabilities
-                info["SSID"] = scanResult.SSID
-                info["BSSID"] = scanResult.BSSID
-                info["FREQUENCY"] = scanResult.frequency
-                info["LEVEL"] = scanResult.level
+                info[CAPABILITIES_PROPERTY_NAME] = scanResult.capabilities
+                info[SSID_PROPERTY_NAME] = scanResult.SSID
+                info[BSSID_PROPERTY_NAME] = scanResult.BSSID
+                info[FREQUENCY_PROPERTY_NAME] = scanResult.frequency
+                info[LEVEL_PROPERTY_NAME] = scanResult.level
             } catch (e: Exception) {
                 Log.e("KK: ", "EXCEPTION during save fields for wifi in WifiScanResult: " + e.cause + ", " + e.message)
             }
@@ -35,16 +41,16 @@ class WifiScanResult(override var context: Context, scanResult: ScanResult?) : M
     }
 
     fun getWifiName() : String {
-        val ssid = info["SSID"]
+        val ssid = info[SSID_PROPERTY_NAME]
         return if(ssid != null) {
             val wifiName: String = ssid as String
             if (!wifiName.isBlank()) {
                 wifiName
             } else {
-                "Unknown_" + System.currentTimeMillis()
+                UNKNOWN_PROPERTY_NAME + System.currentTimeMillis()
             }
         } else {
-            "Unknown_" + System.currentTimeMillis()
+            UNKNOWN_PROPERTY_NAME + System.currentTimeMillis()
         }
     }
 }
